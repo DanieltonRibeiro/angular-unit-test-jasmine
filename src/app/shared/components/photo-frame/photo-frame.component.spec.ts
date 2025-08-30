16c1709d8bed3bde0e2a45f3829805fb3034c218
Danielton <danielton.ribeiro@fmd.ag>
2025-08-29 22:31:58 -0300

    feat(photo frame - teste unitario): cria casos de testes para o contador de like

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,6 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('PhotoFrameComponent', () => {
   let component: PhotoFrameComponent;
@@ -8,18 +14,42 @@ describe('PhotoFrameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoFrameComponent ]
+      declarations: [ PhotoFrameComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PhotoFrameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger
+  (@Output liked) once when called multiple times within debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => {
+      times++;
+    });
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name} should trigger
+  (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => {
+      times++;
+    });
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
 });
