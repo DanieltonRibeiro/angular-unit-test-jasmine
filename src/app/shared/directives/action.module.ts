bca8c4c08db552670a2985a70d33bc6efae2fb39
Danielton <danielton.ribeiro@fmd.ag>
2025-08-30 08:18:32 -0300

    feat(diretivas): cria diretiva para registrar ações do usuário

---
+++ src/app/shared/directives/action.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ActionDirective } from './action.directive';
+
+@NgModule({
+  imports: [],
+  declarations: [ActionDirective],
+  exports: [ActionDirective],
+})
+export class ActionDirectiveModule {}
