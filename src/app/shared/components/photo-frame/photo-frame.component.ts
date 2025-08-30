733ee10419c2a630ab2e791507519cc1eb2f5a78
Danielton <danielton.ribeiro@fmd.ag>
2025-08-29 22:03:35 -0300

    feat(photo frame): adiciona debounce para evitar várias requisições

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,11 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss']
 })
-export class PhotoFrameComponent implements OnInit {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
 
   @Output() public liked: EventEmitter<void> = new EventEmitter();
 
@@ -13,13 +22,29 @@ export class PhotoFrameComponent implements OnInit {
   @Input() public description = '';
   @Input() public likes = 0;
 
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribeSubject: Subject<void> = new Subject();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.debounceSubject.asObservable()
+        .pipe(
+            takeUntil(this.unsubscribeSubject),
+            debounceTime(500)
+        )
+        .subscribe(() => {
+          this.liked.emit();
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeSubject.next();
+    this.unsubscribeSubject.complete();
   }
 
   public like(): void {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 
 }
