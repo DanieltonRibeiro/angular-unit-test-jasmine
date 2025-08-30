fb0d27ef1ba75a2350db19758e68f61e5a600e36
Danielton <danielton.ribeiro@fmd.ag>
2025-08-29 21:12:16 -0300

    feat(photo-frame): cria componente de foto

---
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-frame',
+  templateUrl: './photo-frame.component.html',
+  styleUrls: ['./photo-frame.component.scss']
+})
+export class PhotoFrameComponent implements OnInit {
+
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+
+  @Input() public src = '';
+  @Input() public description = '';
+  @Input() public likes = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+
+}
